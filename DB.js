8d5c182342968ce0fb3b794a430f9c1be6b60b62
Matheus <matheusvitoriosfreitas@gmail.com>
2025-11-05 06:46:55 -0300

    atualização do projeto

--- DB.js
+++ DB.js
@@ -1,41 +1,33 @@
-require("dotenv").config()
-const {Pool} = require("pg")
+const {query} = require("./Con_DB");
 
-const pool = new Pool({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
-    port: process.env.PGPORT
-})
-
-async function query(sql_comand, params) {
-   try {
-        const res = await pool.query(sql_comand, params)
-        return res
-   } catch (err) {
-        return err
-   }
-}
-
-class DB_User_Master {
-    async cadastrar_usuario(valores) {
-        await query("INSERT INTO Usuario (id, email, senha, nome, classe, xp, nivel, moedas)  VALUES ($1, $2, $3, $4, $5, $6, $7, $8)", valores)
+class DB_User {
+    async cadastrar_user(valores) {
+        await query("INSERT INTO Usuario (id, email, senha, nome, classe, xp, nivel, moedas, ativo)  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)", valores)
     }
 
     async listar_user() {
-        const res = await query("SELECT * FROM Usuario")
+        const res = await query("SELECT * FROM Usuario WHERE ativo = true")
         return res.rows
     }
 
     async update_user(coluna, novo_valor, id) {
         await query(`UPDATE Usuario SET ${coluna} = $1 WHERE id = $2`, [novo_valor, id])
     }
+
+    async deletar_user(id) {
+        await query(`UPDATE Usuario SET ativo = false WHERE id = $1`, [id])
+    }
 }
 
 ;(async () => {
-    const user_master = new DB_User_Master() 
-    console.log(await user_master.listar_user())
-    await user_master.update_user("xp", 5, "12345678910")
-    console.log(await user_master.listar_user())
+    //const user_master = new DB_User_Master() 
+    //user_master.cadastrar_user(["12345678910", "matheusvitoriosfreitas@gmail.com", "22022008mV@", "Matheus", "mago", 0, 1, 0, true])
+    //console.log(await user_master.listar_user())
+    //await user_master.update_user("xp", 5, "12345678910")
+    //await user_master.update_user("ativo", "true", "12345678910")
+    //console.log(await user_master.listar_user())
+    //await user_master.deletar_user("12345678910")
+    //console.log(user_master.listar_user())
 })();
+
+module.exports = {DB_User,}
